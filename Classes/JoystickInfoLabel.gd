b000b7644e4573072db20009b1862ff6d5945db8
Andrew n marshall <Anm@anm.me>
2021-01-10 09:41:12 -0800

    Track GUIDs on Joysticks
    The connection based code is still buggy, but looking for and testing
    against joy_guid seems to add reliability.

--- Classes/JoystickInfoLabel.gd
+++ Classes/JoystickInfoLabel.gd
@@ -2,7 +2,8 @@ extends Label
 
 const _MAX_DEVICE_ID = 100
 
-# Array of arrays: The inner array is [device id, device text]
+
+# Array of arrays: The inner array is [device id, guid, device text]
 var _data = []
 
 # Called when the node enters the scene tree for the first time.
@@ -12,24 +13,22 @@ func _ready():
 	ConnectTo.orDie(Input,"joy_connection_changed", self, "_updateJoystickInfo")
 
 func _initialSearchForDevices():
-	var missing_device_count = 0
 	for device_id in _MAX_DEVICE_ID:
-		if Input.is_joy_known(device_id):
-			_data.append([device_id, _build_text(device_id)])
-			missing_device_count = 0
-		else:
-			++missing_device_count
-		if missing_device_count >= 3:
-			return
+		if Input.is_joy_known(device_id) and not Input.get_joy_guid(device_id).empty():
+			_data.append(_build_device_record(device_id))
 
-func _build_text(device_id) -> String:
-	return "Joystick #" + device_id + ": " + Input.get_joy_guid(device_id)
+func _build_device_text(device_id) -> String:
+	return "Joystick #" + str(device_id) + ": " + Input.get_joy_guid(device_id)
+
+func _build_device_record(device_id: int) -> Array:
+	return [device_id, Input.get_joy_guid(device_id), _build_device_text(device_id)]
 
 # Find the index for the device_id in _data
 func _find_index(device_id) -> int:
+	var guid = Input.get_joy_guid(device_id)
 	for i in _data.size():
 		var record = _data[i]
-		if record[i] == device_id:
+		if record[0] == device_id and record[1] == guid:
 			return i
 	return -1  # Not Found
 
@@ -38,12 +37,10 @@ func _find_insertion_point(device_id :int) -> int:
 
 func _updateJoystickInfo(device_id :int, connected :bool):
 	if device_id >= 0:
-		if connected:
-			var device_text = _build_text(device_id)
-			var index = _find_insertion_point(device_id)
-			_data.insert(index, [device_id, device_text])
+		var index = _find_index(device_id)
+		if connected and index > -1:
+			_data.insert(index, _build_device_record(device_id))
 		else:
-			var index = _find_index(device_id)
 			if index != -1:
 				_data.remove(-1)
 	_rebuild_text()
