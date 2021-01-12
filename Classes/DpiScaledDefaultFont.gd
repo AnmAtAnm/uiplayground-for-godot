4efb3b1ba432b7dd67c93fe2dfeca14eed5d7ea4
Andrew n marshall <Anm@anm.me>
2021-01-12 08:15:00 -0800

    Issue: Font too small on external monitor
    Mac: external monitor reports 72dpi by default. This results in fonts that are
    too small under DpiScaledDefaultFont.

--- Classes/DpiScaledDefaultFont.gd
+++ Classes/DpiScaledDefaultFont.gd
@@ -2,6 +2,7 @@ extends Control
 
 export(DynamicFont) var font
 export(float, 1, 288, 1.0) var font_size_points: float = 16.0
+# TODO: Minimum font size as % of viewport height
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,7 +12,7 @@ func _ready():
 		# Find the closest theme definition, falling back on the default theme
 		if not theme:
 			theme = Theme.new()
-			theme.copy_default_theme()  # Fail back
+			theme.copy_default_theme()  # Fallback
 			var parent = get_parent()
 			while parent:
 				var parent_theme = parent.theme as Theme
--- TODO.md
+++ TODO.md
@@ -2,6 +2,8 @@ UI Playground ToDos
 ===================
 
  * PanelContainers that shrink with children
+ * Font size too small on large external monitors
+   * Idea: Set min size as % of window height?
  * Joysticks
    * Map buttons to layout and add on-screen indicators
    * On-screen indicators for analog sticks
